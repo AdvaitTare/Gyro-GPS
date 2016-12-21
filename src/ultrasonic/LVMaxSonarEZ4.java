9372795f0481d541f80e6ef4b1bc18a5d692f951
rcardenas01 <rodrigo.cardenas@frogforce503.org>
2016-12-20 20:21:24 -0500

    Ultrasonic Sensor Files

---
+++ src/ultrasonic/LVMaxSonarEZ4.java
@@ -0,0 +1,26 @@
+package com.team503.lib.util;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+/*=============================================================
+ * +--------------------+--------------+
+ * | LVMaxSonarEZ4.java | FRC Team 503 |
+ * +--------------------+--------------+
+ * 
+ * This is the code for the ultrasonic sensors
+ *=============================================================*/
+public class LVMaxSonarEZ4 
+{
+	private final double VOLTS_PER_INCH = 0.009766;
+	private AnalogInput analogSensor;
+	
+	public LVMaxSonarEZ4(int port)
+	{
+		analogSensor = new AnalogInput(port);
+	}
+	//This next method has not been tested yet
+	public double getDistance()
+	{
+		return analogSensor.getVoltage()/VOLTS_PER_INCH;
+	}
+	
+}
